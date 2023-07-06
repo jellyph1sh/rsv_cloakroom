5310875cd125782701aa3b8a187a4817b230916f
JellyPh1sh <sacha.sorgiati@gmail.com>
2023-07-06 18:05:11 +0200

    :rocket: | Deploy resource structure.

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+
+version "1.0"
+description "Cloakroom system linked to rsv_skin."
+author "JellyPh1sh"
+
+client_scripts {
+    "client/*"
+}
+
+server_scripts {
+    "server/*"
+}
+
+shared_scripts {
+    "shared/*"
+}
+
+dependency "rsv_skin"
